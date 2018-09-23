1ac03d72232fc57b9e56a7b923842a00e8d4d695
Juzz31 <39912769+Juzz31@users.noreply.github.com>
2018-09-23 22:27:44 +0200

    Create ban.js

---
+++ .gitignore/commands/ban.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Je ne trouve pas cet utilisateur...");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Vous n'avez pas la permission");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Cette personne ne peut être bannie");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Utilisateur Banni", `${bUser} with ID ${bUser.id}`)
+    .addField("Banni Par", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Banni Dans", message.channel)
+    .addField("Heure", message.createdAt)
+    .addField("Raison", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "logs");
+    if(!incidentchannel) return message.channel.send("Je ne trouve pas de channel logs");
+
+    message.guild.member(bUser).ban(bReason);
+    message.delete().catch(O_o=>{});
+    incidentchannel.send(banEmbed);
+
+    var userban = message.mentions.users.first();
+    userban.send(`Vous avez été banni de ${message.guild.name} pour ${bReason}`);
+}
+
+module.exports.help = {
+  name:"ban"
+}
