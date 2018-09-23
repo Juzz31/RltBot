4a18c5a3ffea373bfdce82cc9f562df5ff6ef838
Juzz31 <39912769+Juzz31@users.noreply.github.com>
2018-09-23 22:24:26 +0200

    Update clear.js

--- .gitignore/commands/clear.js
+++ .gitignore/commands/clear.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No, you can not do that !");
-  if(!args[0]) return message.channel.send("no");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Vous n'avez pas accès à cette commande");
+  if(!args[0]) return message.channel.send("non !");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
