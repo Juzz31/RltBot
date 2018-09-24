a0fbcc72ccf75a2eb2322375657f8fa4b72c2009
Juzz31 <39912769+Juzz31@users.noreply.github.com>
2018-09-24 19:06:10 +0200

    Update clear.js

--- .gitignore/commands/clear.js
+++ .gitignore/commands/clear.js
@@ -2,12 +2,24 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Vous n'avez pas accès à cette commande");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Vous n'avez pas la permission pour faire cela...");
   if(!args[0]) return message.channel.send("non !");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
 
+  let cEmbed = new Discord.RichEmbed()
+  .setDescription("~Clear~")
+  .setColor("#00BFFF")
+  .addField("Effacés Par", `<@${message.author.id}> avec comme ID ${message.author.id}`)
+  .addField("Nombre de message effacés", args[0])
+  .addField("Effacés Dans", message.channel)
+  .addField("Heure", message.createdAt);
+
+  let incidentchannel = message.guild.channels.find(`name`, "logs");
+  if(!incidentchannel) return message.channel.send("Je ne trouves pas ce channel.");
+
+    incidentchannel.send(cEmbed);
 }
 
 module.exports.help = {
