fab582ee7a2a574acbc09185f4569d6ab16554b9
Juzz31 <39912769+Juzz31@users.noreply.github.com>
2018-09-24 18:25:00 +0200

    Update ban.js

--- .gitignore/commands/ban.js
+++ .gitignore/commands/ban.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     if(!bUser) return message.channel.send("Je ne trouve pas cet utilisateur...");
     let bReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Vous n'avez pas la permission");
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Cette personne ne peut être bannie");
+    if(bUser.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Cette personne ne peut être bannie");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
