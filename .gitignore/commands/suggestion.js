30bd397dfa73c7ad889ba70c09c17d4fa7735c6b
Juzz31 <39912769+Juzz31@users.noreply.github.com>
2018-09-30 12:37:11 +0200

    Create suggestion.js

---
+++ .gitignore/commands/suggestion.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  message.delete();
+
+
+    if(!args[0]) {
+      message.channel.send("Vous devez renseigner une suggestion...");
+      return;
+    }
+
+    message.channel.send("Merci pour votre suggestion d'amélioration, nous la traitons au plus vite");
+
+  const candidMessage = args.join(" ");
+
+  const mdembed = new Discord.RichEmbed()
+  .setTitle("~~Suggestion~~")
+  .addField("auteur :", message.author.tag)
+  .addField('Candidature :', candidMessage);
+
+  message.guild.channels.find('name', "💥-suggestions-bot").send(mdembed)
+  .then((newMessage) => {
+      newMessage.react("👎");
+      newMessage.react("👍");
+      newMessage.pin()
+  });
+
+}
+
+module.exports.help = {
+  name: "suggestion"
+}
